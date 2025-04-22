5fce278778136d44fad580cdbf35654f05bb0643
schromp <schromp@echsen.club>
2025-04-22 13:46:35 +0200

    sparrow: init

--- machines/sparrow/configuration.nix
+++ machines/sparrow/configuration.nix
@@ -1,7 +1,27 @@
-{
+{lib, ...}: {
   imports = [
-
+    ../../modules/disko.nix
+    ../../modules/shared.nix
   ];
 
-  # New machine!
+  users.users.user.name = "lk";
+
+  disko.devices.disk.main.device = "/dev/disk/by-id/wwn-0x50026b767b01bd9f";
+
+  clan.core.networking.targetHost = "root@sparrow";
+
+  users.users.root.openssh.authorizedKeys.keys = [
+    ''
+      ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJRat12+538VwG/IAv5R4AjdNYz/GATO7ULQnXtYC2HK lk@tower
+    ''
+  ];
+
+  networking = {
+    interfaces."enp0s31f6" = {
+      ipv4.addresses = [{
+        address = "192.168.178.2";
+        prefixLength = 24;
+      }];
+    };
+  };
 }
