9cded302e471b98b26e0e0bbe347876e4dc4ef45
schromp <lennart@koziollek.com>
2025-08-10 15:10:40 +0200

    working netbird login for phone

--- machines/cloudy/nginx.nix
+++ machines/cloudy/nginx.nix
@@ -25,15 +25,22 @@
         };
       };
       "netbird.echsen.club" = {
-        useACMEHost = "netbird.echsen.club";
         forceSSL = true;
-        locations."/api" = {
-          proxyPass = "http://127.0.0.1:8011";
-        };
-        locations."/management.ManagementService/" = {
-          proxyPass = "http://127.0.0.1:8011";
-          proxyWebsockets = true;
-          extraConfig = ''
+        useACMEHost = "netbird.echsen.club";
+        extraConfig = ''
+          proxy_set_header        X-Real-IP $remote_addr;
+          proxy_set_header        X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header        X-Scheme $scheme;
+          proxy_set_header        X-Forwarded-Proto https;
+          proxy_set_header        X-Forwarded-Host $host;
+          grpc_set_header         X-Forwarded-For $proxy_add_x_forwarded_for;
+        '';
+        locations = {
+          "/api".proxyPass = "http://127.0.0.1:8011";
+
+          "/management.ManagementService/".extraConfig = ''
+            # This is necessary so that grpc connections do not get closed early
+            # see https://stackoverflow.com/a/67805465
             client_body_timeout 1d;
 
             grpc_pass grpc://127.0.0.1:8011;
@@ -42,20 +49,18 @@
             grpc_socket_keepalive on;
           '';
         };
-        locations."/signalexchange.SignalExchange/" = {
-          proxyPass = "http://127.0.0.1:8012";
-          proxyWebsockets = true;
-          extraConfig = ''
-            client_body_timeout 1d;
+        locations."/signalexchange.SignalExchange/".extraConfig = ''
+          # This is necessary so that grpc connections do not get closed early
+          # see https://stackoverflow.com/a/67805465
+          client_body_timeout 1d;
 
-            grpc_pass grpc://192.168.178.2:8012;
-            grpc_read_timeout 1d;
-            grpc_send_timeout 1d;
-            grpc_socket_keepalive on;
-          '';
-        };
+          grpc_pass grpc://127.0.0.1:8012;
+          grpc_read_timeout 1d;
+          grpc_send_timeout 1d;
+          grpc_socket_keepalive on;
+        '';
         locations."/relay".extraConfig = ''
-          proxy_pass http://192.168.178:33080;
+          proxy_pass http://127.0.0.1:33080/relay;
 
           # WebSocket support
           proxy_http_version 1.1;
@@ -70,7 +75,6 @@
           # Handle upstream errors
           proxy_next_upstream error timeout invalid_header http_500 http_502 http_503 http_504;
         '';
-        # NOTE: the dashboard is configured through the nix module
       };
     };
   };
