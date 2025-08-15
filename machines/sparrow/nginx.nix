06797e5ec2afc62c44ce681f875961ec0952f4d8
schromp <schromp@echsen.club>
2025-08-15 23:54:17 +0200

    cleanup, enable nextcloud

--- machines/sparrow/nginx.nix
+++ machines/sparrow/nginx.nix
@@ -21,6 +21,10 @@
           proxyWebsockets = true;
         };
       };
+      "nextcloud.echsen.club" = {
+        useACMEHost = "nextcloud.echsen.club";
+        forceSSL = true;
+      };
       "audiobookshelf.echsen.club" = {
         useACMEHost = "audiobookshelf.echsen.club";
         forceSSL = true;
@@ -77,54 +81,6 @@
           proxyWebsockets = true;
         };
       };
-      "netbird.echsen.club" = {
-        useACMEHost = "netbird.echsen.club";
-        forceSSL = true;
-        locations."/api" = {
-          proxyPass = "http://127.0.0.1:8011";
-        };
-        locations."/management.ManagementService/" = {
-          proxyPass = "http://127.0.0.1:8011";
-          proxyWebsockets = true;
-          extraConfig = ''
-            client_body_timeout 1d;
-
-            grpc_pass grpc://127.0.0.1:8011;
-            grpc_read_timeout 1d;
-            grpc_send_timeout 1d;
-            grpc_socket_keepalive on;
-          '';
-        };
-        locations."/signalexchange.SignalExchange/" = {
-          proxyPass = "http://127.0.0.1:8012";
-          proxyWebsockets = true;
-          extraConfig = ''
-            client_body_timeout 1d;
-
-            grpc_pass grpc://192.168.178.2:8012;
-            grpc_read_timeout 1d;
-            grpc_send_timeout 1d;
-            grpc_socket_keepalive on;
-          '';
-        };
-        locations."/relay".extraConfig = ''
-          proxy_pass http://192.168.178:33080;
-
-          # WebSocket support
-          proxy_http_version 1.1;
-          proxy_set_header Upgrade $http_upgrade;
-          proxy_set_header Connection "Upgrade";
-
-          # Timeout settings
-          proxy_read_timeout 3600s;
-          proxy_send_timeout 3600s;
-          proxy_connect_timeout 60s;
-
-          # Handle upstream errors
-          proxy_next_upstream error timeout invalid_header http_500 http_502 http_503 http_504;
-        '';
-        # # NOTE: the dashboard is configured through the nix module
-      };
       # "signal-sparrow.netbird.echsen.club" = {
       #   useACMEHost = "signal-sparrow.netbird.echsen.club";
       #   forceSSL = true;
@@ -150,77 +106,6 @@
           proxyWebsockets = true;
         };
       };
-    };
-  };
-
-  security.acme = {
-    acceptTerms = true;
-    defaults.email = "server@echsen.club";
-
-    certs."zitadel.echsen.club" = {
-      group = "nginx";
-      dnsProvider = "cloudflare";
-      environmentFile = config.clan.core.vars.generators."acme-cloudflare-api-key".files."acme-cf-env".path;
-    };
-    certs."netbird.echsen.club" = {
-      group = "nginx";
-      dnsProvider = "cloudflare";
-      environmentFile = config.clan.core.vars.generators."acme-cloudflare-api-key".files."acme-cf-env".path;
-    };
-    certs."turn-sparrow.netbird.echsen.club" = {
-      group = "nginx";
-      dnsProvider = "cloudflare";
-      environmentFile = config.clan.core.vars.generators."acme-cloudflare-api-key".files."acme-cf-env".path;
-    };
-    certs."jellyfin.echsen.club" = {
-      group = "nginx";
-      dnsProvider = "cloudflare";
-      environmentFile = config.clan.core.vars.generators."acme-cloudflare-api-key".files."acme-cf-env".path;
-    };
-    certs."audiobookshelf.echsen.club" = {
-      group = "nginx";
-      dnsProvider = "cloudflare";
-      environmentFile = config.clan.core.vars.generators."acme-cloudflare-api-key".files."acme-cf-env".path;
-    };
-    certs."jellyseerr.echsen.club" = {
-      group = "nginx";
-      dnsProvider = "cloudflare";
-      environmentFile = config.clan.core.vars.generators."acme-cloudflare-api-key".files."acme-cf-env".path;
-    };
-    certs."sonarr.echsen.club" = {
-      group = "nginx";
-      dnsProvider = "cloudflare";
-      environmentFile = config.clan.core.vars.generators."acme-cloudflare-api-key".files."acme-cf-env".path;
-    };
-    certs."radarr.echsen.club" = {
-      group = "nginx";
-      dnsProvider = "cloudflare";
-      environmentFile = config.clan.core.vars.generators."acme-cloudflare-api-key".files."acme-cf-env".path;
-    };
-    certs."sabnzbd.echsen.club" = {
-      group = "nginx";
-      dnsProvider = "cloudflare";
-      environmentFile = config.clan.core.vars.generators."acme-cloudflare-api-key".files."acme-cf-env".path;
-    };
-    certs."prowlarr.echsen.club" = {
-      group = "nginx";
-      dnsProvider = "cloudflare";
-      environmentFile = config.clan.core.vars.generators."acme-cloudflare-api-key".files."acme-cf-env".path;
-    };
-    certs."readarr.echsen.club" = {
-      group = "nginx";
-      dnsProvider = "cloudflare";
-      environmentFile = config.clan.core.vars.generators."acme-cloudflare-api-key".files."acme-cf-env".path;
-    };
-    certs."pdf.echsen.club" = {
-      group = "nginx";
-      dnsProvider = "cloudflare";
-      environmentFile = config.clan.core.vars.generators."acme-cloudflare-api-key".files."acme-cf-env".path;
-    };
-    certs."kavita.echsen.club" = {
-      group = "nginx";
-      dnsProvider = "cloudflare";
-      environmentFile = config.clan.core.vars.generators."acme-cloudflare-api-key".files."acme-cf-env".path;
     };
   };
 }
