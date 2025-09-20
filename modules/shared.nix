5eef3db8a8a7e449f39eaff81fa3ed1bf8bc50ce
schromp <schromp@echsen.club>
2025-09-20 18:35:04 +0200

    auto garbage collection

--- modules/shared.nix
+++ modules/shared.nix
@@ -3,7 +3,8 @@
   clan-core,
   pkgs,
   ...
-}: {
+}:
+{
   # Locale service discovery and mDNS
   # services.avahi.enable = false;
 
@@ -12,12 +13,18 @@
     group = "lk";
     openssh.authorizedKeys.keys = config.users.users.root.openssh.authorizedKeys.keys;
   };
-  users.groups.lk = {};
+  users.groups.lk = { };
 
   environment.systemPackages = with pkgs; [
     vim
     git
     nettools
   ];
+
+  nix.gc.automatic = true;
+  nix.gc.dates = "weekly";
+  nix.optimise.automatic = true;
+  nix.optimise.dates = [ "03:45" ]; # Optional; allows customizing optimisation schedule
+
   clan.core.settings.state-version.enable = true;
 }
