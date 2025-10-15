56e27b35706a4bac2e5da9ba375da4920c81fb01
schromp <schromp@echsen.club>
2025-10-15 13:47:38 +0200

    added tangled knot

---
+++ machines/cloudy/tangled.nix
@@ -0,0 +1,19 @@
+{ inputs, ... }:
+{
+  imports = [
+    inputs.tangled.nixosModules.knot
+  ];
+
+  services.tangled-knot = {
+    enable = true;
+    gitUser = "Echsmachina";
+    openFirewall = false;
+    motd = ''
+      Yarrr write some code
+    '';
+    server = {
+      owner = "did:plc:ltd46vfzdow3gesucfihrqc2";
+      hostname = "knot.echsen.club";
+    };
+  };
+}
