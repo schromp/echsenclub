e87ff94f39fd50e58beb187c77a9382cc64778b6
schromp <lennart@koziollek.com>
2025-10-16 11:33:07 +0200

    add tangled spindle

--- machines/cloudy/tangled.nix
+++ machines/cloudy/tangled.nix
@@ -2,6 +2,7 @@
 {
   imports = [
     inputs.tangled.nixosModules.knot
+    inputs.tangled.nixosModules.spindle
   ];
 
   services.tangled-knot = {
@@ -14,6 +15,15 @@
     server = {
       owner = "did:plc:ltd46vfzdow3gesucfihrqc2";
       hostname = "knot.echsen.club";
+    };
+  };
+
+  services.tangled-spindle = {
+    enable = true;
+
+    server = {
+      hostname = "spindle.echsen.club";
+      owner = "did:plc:ltd46vfzdow3gesucfihrqc2";
     };
   };
 }
