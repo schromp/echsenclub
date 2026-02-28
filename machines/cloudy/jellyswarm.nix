99f53d0497acc5e6c4347442bf8d6830a4ba4347
schromp <schromp@echsen.club>
2026-02-28 18:19:04 +0100

    add jellyswarrm

---
+++ machines/cloudy/jellyswarm.nix
@@ -0,0 +1,49 @@
+{inputs, pkgs, config, lib, ...}: {
+  imports = [
+    inputs.jellyswarm.nixosModules.default
+  ];
+
+  services.jellyswarrm = {
+    enable = true;
+    package = inputs.jellyswarm.packages.${pkgs.system}.default.overrideAttrs 
+      (finalAttrs: previousAttrs: {      
+        src = pkgs.fetchFromGitHub {
+          owner = "LLukas22";
+          repo = "Jellyswarrm";
+          rev = "v0.2.0";
+          sha256 = "sha256-bf+HiZLS54abDV9wW/MZQT/UJrtUQMlmFcmN+5T2FYU=";
+        };
+        cargoDeps = pkgs.rustPlatform.importCargoLock {
+          lockFile = finalAttrs.src + "/Cargo.lock";
+          allowBuiltinFetchGit = true;
+        };
+        cargoHash = null;
+      });
+    host = "127.0.0.1";
+    port = 3030;
+    username = "admin";
+    passwordFile = config.clan.core.vars.generators."jellyswarm-admin-password".files."password".path;
+    extraEnvironment = {
+      JELLYSWARM_SERVER_NAME = "echsflix";
+      JELLYSWARM_INCLUDE_SERVER_NAME_IN_MEDIA = "true";
+      JELLYSWARRM_PUBLIC_ADDRESS = "jellyswarm.echsen.club";
+      # JELLYSWARRM_PRECONFIGURED_SERVERS = [];
+    };
+  };
+
+  clan.core.vars.generators = {
+    "jellyswarm-admin-password" = {
+      files."password" = {
+        secret = true;
+      };
+      runtimeInputs = with pkgs; [
+        openssl
+      ];
+      script = ''
+        openssl rand -base64 32 > $out/password
+      '';
+      share = false;
+    };
+  };
+  
+}
