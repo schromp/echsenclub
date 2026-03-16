52a9125a81a4307e12b1b9c62d12aa6a6297e0af
schromp <schromp@echsen.club>
2026-03-17 00:16:38 +0100

    doorman dns fixes

--- machines/doorman/configuration.nix
+++ machines/doorman/configuration.nix
@@ -1,11 +1,39 @@
-{
+{lib, ...}: {
   imports = [
     ./caddy.nix
     ./lldap.nix
     ./authelia/authelia.nix
   ];
 
-  networking.firewall.allowedTCPPorts = [ 80 443 ];
+  networking.firewall.allowedTCPPorts = [
+    80
+    443
+  ];
+
+  networking.hosts = {
+    "127.0.0.1" = [
+      "netbird.echsen.club"
+      "sso2.echsen.club"
+    ];
+    "::1" = [ "localhost" ];
+  };
+
+  services.resolved = {
+    enable = true; # Keep it enabled if you need it
+    settings = {
+      Resolve = {
+        # This ensures resolved looks at /etc/hosts BEFORE trying DNS
+        ReadEtcHosts = true;
+        # This can help stop it from getting clever with IPv6 loopbacks
+        LLMNR = "no";
+        MulticastDNS = "no";
+        # Forces preference for IPv4
+        # Note: 'ipv4' here tells systemd to prefer A records
+        ResolveUnicastSingleLabel = "yes";
+      };
+    };
+  };
+
 
   clan.core.networking.targetHost = "root@doorman";
 }
